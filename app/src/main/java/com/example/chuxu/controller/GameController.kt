f032b4ca7dec05b5b3e2dbcf36ac85c2c0efba36
TetraSsky <tetrasky.bat@gmail.com>
2024-04-24 11:03:46 +0400

    Changer structure BDD - Intégrer scrap des informations sur la recherche

--- app/src/main/java/com/example/chuxu/controller/GameController.kt
+++ app/src/main/java/com/example/chuxu/controller/GameController.kt
@@ -7,28 +7,17 @@ import kotlinx.coroutines.withContext
 object GameController {
 
     // Fonction pour insérer un jeu dans la base de données
-    suspend fun insertGame(id: Int, name: String, platformId: Int, type: String) {
+    suspend fun insertGame(id: Int, name: String) {
         return withContext(Dispatchers.IO) {
             try {
                 val connection = DatabaseManager.getConnection()
                 connection?.let {
-                    val query = "SELECT COUNT(*) FROM Games WHERE GameID=?"
-                    val statement = it.prepareStatement(query)
-                    statement.setInt(1, id)
-                    val result = statement.executeQuery()
-
-                    if (!result.next()) {
-                        val query = "INSERT INTO Games (GameID, Nom, PlateformeID, GameType) VALUES (?, ?, ?, ?)"
+                        val query = "INSERT INTO Games (GameID, Nom) VALUES (?, ?)"
                         val statement = it.prepareStatement(query)
                         statement.setInt(1, id)
                         statement.setString(2, name)
-                        statement.setInt(3, platformId)
-                        statement.setString(4, type)
                         statement.executeUpdate()
                         statement.close()
-                    } else {
-                        statement.close()
-                    }
                 }
             } catch (e: Exception) {
                 e.printStackTrace()
