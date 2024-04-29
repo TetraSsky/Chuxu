3b390430abad4053dc8dca5a2f887d141bad46cd
TetrasSky <tetrasky.bat@gmail.com>
2024-04-29 08:06:33 +0400

    Fixs

--- app/src/main/java/com/example/chuxu/view/Activity3.kt
+++ app/src/main/java/com/example/chuxu/view/Activity3.kt
@@ -9,6 +9,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.ActionBarDrawerToggle
@@ -29,8 +30,25 @@ class Activity3 : AppCompatActivity() {
     private lateinit var passwordEditText: EditText
     private lateinit var passwordVerifEditText: EditText
     private lateinit var nicknameEditText: EditText
-    private val inappropriateNicknames = listOf("negro", "négro", "nègre")
-    private lateinit var connectloadingView: View
+    private val inappropriateNicknames = listOf(
+        // Français
+        "negro", "négro", "nègre", "BlancSuprémaciste", "Raciste", "KKK", "Esclavage", "Aryan", "Nazi", "Hitler", "Fasciste", "Colonialiste", "Ségrégation", "SuprématieBlanche",
+        "Violence", "Haine", "Terroriste", "Pornographie", "Porno", "Automutilation", "Suicide",
+        "Trump", "Biden", "Macron", "Poutine", "XiJinping", "Erdogan", "KimJongUn", "Assad", "Netanyahu", "Merkel", "Laden", "Al-Qaïda",
+
+        // Anglais
+        "WhiteSupremacist", "Racist", "Slavery", "Aryan", "Nazi", "Hitler", "Fascist", "Colonialist", "Segregation", "WhiteSupremacy",
+        "Violence", "Hate", "Terrorist", "Pornography", "SelfHarm", "Sex", "Fuck",
+
+        // Espagnol
+        "BlancoSupremacista", "Racista", "Esclavitud", "Nazi", "Hitler", "Fascista", "Colonialista", "Segregación",
+        "Terrorista", "Pornografía", "Automutilación", "Suicidio",
+    )
+    private var isShowingLoadingView = false
+    private lateinit var loadingView: View
+    private lateinit var loadingTextView1: TextView
+    private lateinit var loadingTextView2: TextView
+    private lateinit var rootView: ViewGroup
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -59,7 +77,7 @@ class Activity3 : AppCompatActivity() {
             } else if (!isNicknameValid(nickname)) {
                 Toast.makeText(this, "Pseudo invalide.", Toast.LENGTH_SHORT).show()
             } else {
-                showLoadingView()
+                showLoadingView("Connexion...", "Veuillez patienter...")
                 val encryptedPassword = UserController.encryptPassword(password)
                 RegisterUserTask().execute(email, encryptedPassword, nickname)
             }
@@ -142,14 +160,29 @@ class Activity3 : AppCompatActivity() {
         }
     }
 
-    // Fonction pour inflate la page actuelle et rajouter un chargement
-    private fun showLoadingView() {
-        connectloadingView = layoutInflater.inflate(R.layout.connect_loading_screen, findViewById(android.R.id.content), false)
-        (findViewById<ViewGroup>(android.R.id.content)).addView(connectloadingView)
+    private fun showLoadingView(text1: String, text2: String) {
+        // Rajouter une vérification (Pour éviter plusieurs loading screen en même temps)
+        if (!isShowingLoadingView) {
+            isShowingLoadingView = true
+            loadingView = layoutInflater.inflate(R.layout.loading_screen, null)
+            loadingTextView1 = loadingView.findViewById(R.id.loadingtextView1)
+            loadingTextView2 = loadingView.findViewById(R.id.loadingtextView2)
+            loadingTextView1.text = text1
+            loadingTextView2.text = text2
+            rootView.addView(loadingView)
+        } else {
+            // Mettre à jour le texte de l'écran de chargement existant
+            updateLoadingView(text1, text2)
+        }
     }
 
-    // Fonction pour cacher la page de chargement
+    private fun updateLoadingView(text1: String, text2: String) {
+        loadingTextView1.text = text1
+        loadingTextView2.text = text2
+    }
+
     private fun hideLoadingView() {
-        (findViewById<ViewGroup>(android.R.id.content)).removeView(connectloadingView)
+        isShowingLoadingView = false
+        rootView.removeView(loadingView)
     }
 }
