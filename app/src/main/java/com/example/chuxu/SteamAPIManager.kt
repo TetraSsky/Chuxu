36044d005c4a0cf85765488a58131d6efed9e25e
TetraSsky <tetrasky.bat@gmail.com>
2024-04-30 16:35:47 +0400

    Ajouts : - Dans le cas où l'application rencontre l'erreur 429 : Mettre en arrêt la recherche pendant 2 minutes avant de renvoyer des requêtes - Fixs de l'affichage pour tablette - Résolution du crash sur page d'Inscription/Connexion

--- app/src/main/java/com/example/chuxu/SteamAPIManager.kt
+++ app/src/main/java/com/example/chuxu/SteamAPIManager.kt
@@ -1,38 +1,59 @@
 package com.example.chuxu
 
+import com.example.chuxu.SteamAPIManager.RateLimitInterceptor.CALL_INTERVAL
 import com.example.chuxu.SteamAPIManager.RateLimitInterceptor.REQUEST_INTERVAL
-import com.example.chuxu.SteamAPIManager.RateLimitInterceptor.REQUEST_LIMIT
 import com.google.gson.annotations.SerializedName
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.withContext
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
 import java.lang.System.currentTimeMillis
-import java.util.concurrent.TimeUnit
 
+/**
+ * Service pour accéder aux fonctionnalités de l'API Steam.
+ */
 interface SteamService {
+    /**
+     * Obtenir la liste des applications Steam.
+     */
     @GET("ISteamApps/GetAppList/v2/")
     suspend fun getAppList(): AppListResponse
 
+    /**
+     * Obtenir les détails d'une application Steam.
+     *
+     * @param appId Identifiant de l'application.
+     * @return Les détails de l'application.
+     */
     @GET("https://store.steampowered.com/api/appdetails")
-    suspend fun getAppDetails(@Query("appids") appId: Int): Map<String, GameDetails>
+    suspend fun getAppDetails(@Query("appids") appId: Int): Response<Map<String, GameDetails>>
 }
 
+/**
+ * Réponse de la liste des applications Steam.
+ */
 data class AppListResponse(
     @SerializedName("applist")
     val appList: AppList
 )
 
+/**
+ * Liste des applications Steam.
+ */
 data class AppList(
     @SerializedName("apps")
     val apps: List<App>
 )
 
+/**
+ * Application Steam.
+ */
 data class App(
     @SerializedName("appid")
     val appId: Int,
@@ -40,11 +61,17 @@ data class App(
     val name: String
 )
 
+/**
+ * Détails d'un jeu Steam.
+ */
 data class GameDetails(
     @SerializedName("data")
     val data: GameData
 )
 
+/**
+ * Données d'un jeu Steam.
+ */
 data class GameData(
     @SerializedName("name")
     val name: String,
@@ -58,11 +85,17 @@ data class GameData(
     val priceOverview: PriceOverview?
 )
 
+/**
+ * Vue d'ensemble du prix d'un jeu Steam.
+ */
 data class PriceOverview(
     @SerializedName("final_formatted")
     val price: String
 )
 
+/**
+ * Gestionnaire de l'API Steam.
+ */
 object SteamAPIManager {
     private const val BASE_URL = "https://api.steampowered.com/"
 
@@ -76,36 +109,35 @@ object SteamAPIManager {
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 
+    /**
+     * Service pour accéder aux fonctionnalités de l'API Steam.
+     */
     val steamService: SteamService = retrofit.create(SteamService::class.java)
-
 
     /**
      * Intercepteur pour limiter le débit des requêtes vers l'API Steam.
      * Cet intercepteur garantit que le nombre de requêtes ne dépasse pas la limite spécifiée.
      */
     object RateLimitInterceptor : Interceptor {
-        const val REQUEST_LIMIT = 200 // Limite de requêtes par minute
-        const val REQUEST_INTERVAL = 120000L // Intervalle entre les requêtes en millisecondes
+        const val REQUEST_LIMIT = 200
+        const val REQUEST_INTERVAL = 120000L
+        const val CALL_INTERVAL = 1000L
 
         private val requestQueue = mutableListOf<Long>()
 
         override fun intercept(chain: Interceptor.Chain): okhttp3.Response {
-            val currentTime = System.currentTimeMillis()
+            val currentTime = currentTimeMillis()
 
             requestQueue.removeAll { currentTime - it > REQUEST_INTERVAL }
 
             while (requestQueue.size >= REQUEST_LIMIT) {
-                Thread.sleep(1000) // Attendre une seconde
+                Thread.sleep(1000)
                 requestQueue.removeAll { currentTime - it > REQUEST_INTERVAL }
             }
-
             requestQueue.add(currentTime)
-
-            // Continuer avec la requête normalement
             return chain.proceed(chain.request())
         }
     }
-
 
     /**
      * Recherche des jeux sur Steam correspondant à un certain terme de recherche.
@@ -116,21 +148,34 @@ object SteamAPIManager {
     suspend fun searchGames(query: String): List<GameData> {
         return withContext(Dispatchers.IO) {
             try {
-                // Obtenir la liste des jeux correspondant à la requête de recherche
                 val response = steamService.getAppList()
                 val games = mutableListOf<GameData>()
 
                 response.appList.apps.forEach { app ->
                     if (app.name.contains(query, ignoreCase = true)) {
-                        println("Searching for game with appId: ${app.appId}")
-                        delay(1000)
-                        // Obtenir les détails du jeu
+
                         val detailsResponse = steamService.getAppDetails(app.appId)
-                        val details = detailsResponse[app.appId.toString()]?.data
-                        println("Scrapped data: $details")
-                        details?.let {
-                            games.add(it)
+
+                        if (detailsResponse.isSuccessful) {
+                            val responseBody = detailsResponse.body()
+                            responseBody?.let { body ->
+                                val details = body[app.appId.toString()]?.data
+                                details?.let {
+                                    games.add(it)
+                                }
+                            }
+                        } else if (detailsResponse.code() == 429) {
+                            delay(REQUEST_INTERVAL)
+                            val detailsResponseRetry = steamService.getAppDetails(app.appId)
+                            val responseBody = detailsResponseRetry.body()
+                            responseBody?.let { body ->
+                                val details = body[app.appId.toString()]?.data
+                                details?.let {
+                                    games.add(it)
+                                }
+                            }
                         }
+                        delay(CALL_INTERVAL)
                     }
                 }
                 games
