b72de482494d0e41f3c14983536b7977023fb3ec
TetraSsky <tetrasky.bat@gmail.com>
2024-05-03 13:19:25 +0400

    Ajouts : - Ajout chargement au moment du fetch des reviews

--- app/src/main/java/com/example/chuxu/view/Activity7.kt
+++ app/src/main/java/com/example/chuxu/view/Activity7.kt
@@ -2,6 +2,9 @@ package com.example.chuxu.view
 
 import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.ActionBarDrawerToggle
@@ -16,13 +19,18 @@ import kotlinx.coroutines.launch
 
 class Activity7 : AppCompatActivity() {
 
+    private var isShowingLoadingView = false
+    private lateinit var loadingView: View
+    private lateinit var loadingTextView1: TextView
+    private lateinit var loadingTextView2: TextView
+
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.reviews)
 
         val appId = intent.getIntExtra("appId", 0)
-
+        showLoadingView("Nous récupérons les avis...","Un instant...")
         fetchGameReviews(appId)
     }
 
@@ -31,9 +39,11 @@ class Activity7 : AppCompatActivity() {
         CoroutineScope(Dispatchers.Main).launch {
             val reviews = UserController.fetchGameReviews(appId)
             if (reviews.isEmpty()) {
+                hideLoadingView()
                 Toast.makeText(this@Activity7, "Aucun avis disponible pour ce jeu.", Toast.LENGTH_SHORT).show()
                 finish()
             } else {
+                hideLoadingView()
                 displayReviews(reviews)
             }
         }
@@ -45,6 +55,30 @@ class Activity7 : AppCompatActivity() {
         adapter.setData(reviews)
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(this)
+    }
+
+    private fun showLoadingView(text1: String, text2: String) {
+        if (!isShowingLoadingView) {
+            isShowingLoadingView = true
+            loadingView = layoutInflater.inflate(R.layout.loading_screen, findViewById(android.R.id.content), false)
+            loadingTextView1 = loadingView.findViewById(R.id.loadingtextView1)
+            loadingTextView2 = loadingView.findViewById(R.id.loadingtextView2)
+            loadingTextView1.text = text1
+            loadingTextView2.text = text2
+            (findViewById<ViewGroup>(android.R.id.content)).addView(loadingView)
+        } else {
+            updateLoadingView(text1, text2)
+        }
+    }
+
+    private fun hideLoadingView() {
+        isShowingLoadingView = false
+        (findViewById<ViewGroup>(android.R.id.content)).removeView(loadingView)
+    }
+
+    private fun updateLoadingView(text1: String, text2: String) {
+        loadingTextView1.text = text1
+        loadingTextView2.text = text2
     }
 }
 
